496c980c7aa5329b97e698795dec29bf98856621
aew2sbee <aew2sbee@gmail.com>
2026-04-16 21:54:04 +0900

    index.htmlにmetaタグを追加し、server.tsのポート番号を6000から3001に変更

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,8 @@
 import express, { Request, Response, NextFunction } from 'express';
 const app = express();
-const port = 6000;
+const port = 3001;
 
 app.use(express.static('public'));
-
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.end('トップページ');
-});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
